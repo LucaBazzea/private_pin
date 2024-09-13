71f556595826a6159d143fa1d1a64f0ae4529d61
luca <auth@luca.cx>
2024-09-13 21:10:49 +0200

    Sidebar widget

---
+++ lib/main_menu.dart
@@ -0,0 +1,15 @@
+import "package:hidden_drawer_menu/hidden_drawer_menu.dart";
+
+class MainMenu extends StatefulWidget {
+  const MainMenu({Key? key}) : super(key: key);
+
+  @override
+  State<MainMenu> createState() => _MainMenuState();
+}
+
+class _MainMenuState extends State<MainMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return HiddenDrawerMenu();
+  }
+}
