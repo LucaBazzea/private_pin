08deb8c2bc372104d46c4d3aa304df215ea3ebfc
luca <auth@luca.cx>
2024-09-18 18:12:58 +0200

    Feed block component

---
+++ lib/components/feed_block.dart
@@ -0,0 +1,36 @@
+import "package:flutter/material.dart";
+
+class Block extends StatelessWidget {
+  final String username;
+  final double lat;
+  final double lon;
+  final String lastOnline;
+
+  const Block(
+      {Key? key,
+      required this.username,
+      required this.lat,
+      required this.lon,
+      required this.lastOnline})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 2.0),
+        child: Container(
+            height: 250,
+            color: Colors.blueGrey,
+            child: Expanded(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                  Column(children: [
+                    Text(username),
+                    Text("Last Online: $lastOnline"),
+                  ]),
+                  Text("$lat, $lon")
+                ]))));
+  }
+}
