98407a8c1a2edf26e54f4cd7d9e09867d536bd44
luca <auth@luca.cx>
2024-09-18 18:54:19 +0200

    List of friends

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,14 +1,31 @@
 import "package:flutter/material.dart";
 
+import "package:private_pin/components/feed_block.dart";
+
 class Home extends StatelessWidget {
-  const Home({super.key});
+  final List _items = [
+    {
+      "username": "GordonFreeman",
+      "last_online": "14:42",
+      "lat": 1.234,
+      "lon": 1.234
+    },
+    {"username": "Alyx", "last_online": "14:42", "lat": 1.234, "lon": 1.234},
+    {"username": "CptEli", "last_online": "14:42", "lat": 1.234, "lon": 1.234},
+    {"username": "REDACTED", "last_online": "14:42", "lat": 1.234, "lon": 1.234}
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("H O M E"),
-      ),
-    );
+    return Scaffold(
+        body: ListView.builder(
+            itemCount: _items.length,
+            itemBuilder: (context, index) {
+              return Block(
+                  username: _items[index]["username"],
+                  lastOnline: _items[index]["last_online"],
+                  lat: _items[index]["lat"],
+                  lon: _items[index]["lon"]);
+            }));
   }
 }
