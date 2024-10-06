069ccd676124f157813cd64705f9893039453e46
luca <auth@luca.cx>
2024-10-06 18:42:36 +0200

    Call API for user

---
+++ lib/services/api.dart
@@ -0,0 +1,42 @@
+import "dart:convert";
+import "package:http/http.dart" as http;
+
+Future<User> getUser() async {
+  final response =
+      await http.get(Uri.parse("http://127.0.0.1:8000/app/get-user?id=1"));
+
+  if (response.statusCode == 200) {
+    return User.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+  } else {
+    throw Exception("Failed to load user");
+  }
+}
+
+class User {
+  final int id;
+  final String username;
+  final String lastOnline;
+  final double lat;
+  final double lon;
+
+  User(
+      {required this.id,
+      required this.username,
+      required this.lastOnline,
+      required this.lat,
+      required this.lon});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+        id: json["id"],
+        username: json["username"],
+        lastOnline: json["last_online"],
+        lat: json["lat"],
+        lon: json["lon"]);
+  }
+}
+
+main() async {
+  final user = await getUser();
+  print(user.username);
+}
