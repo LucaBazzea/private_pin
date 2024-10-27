65a14c460633ee67271a7b2d211e3d73a2b95ccf
luca <auth@luca.cx>
2024-10-27 18:15:13 +0200

    Get connections from API

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -2,39 +2,9 @@ import "package:flutter/material.dart";
 import "package:go_router/go_router.dart";
 
 import "package:private_pin/components/feed_block.dart";
+import "package:private_pin/services/api.dart";
 
 class Home extends StatelessWidget {
-  final List _items = [
-    {
-      "id": "1",
-      "username": "GordonFreeman",
-      "last_online": "14:42",
-      "lat": 1.234,
-      "lon": 1.234
-    },
-    {
-      "id": "2",
-      "username": "Alyx",
-      "last_online": "14:42",
-      "lat": 1.234,
-      "lon": 1.234
-    },
-    {
-      "id": "3",
-      "username": "CptEli",
-      "last_online": "14:42",
-      "lat": 1.234,
-      "lon": 1.234
-    },
-    {
-      "id": "4",
-      "username": "REDACTED",
-      "last_online": "14:42",
-      "lat": 1.234,
-      "lon": 1.234
-    }
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,17 +15,28 @@ class Home extends StatelessWidget {
           backgroundColor: Colors.blueGrey,
           elevation: 0,
         ),
-        body: ListView.builder(
-            itemCount: _items.length,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                  onTap: () => context.goNamed("map",
-                      pathParameters: {"userID": _items[index]["id"]}),
-                  child: Block(
-                      username: _items[index]["username"],
-                      lastOnline: _items[index]["last_online"],
-                      lat: _items[index]["lat"],
-                      lon: _items[index]["lon"]));
+        body: FutureBuilder(
+            future: getConnections("1"),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return ListView.builder(
+                    itemCount: connections.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                          onTap: () => context.goNamed("map", pathParameters: {
+                                "userID": connections[index].id.toString()
+                              }),
+                          child: Block(
+                            lat: connections[index].lat,
+                            lon: connections[index].lon,
+                            username: connections[index].username,
+                            lastOnline:
+                                connections[index].lastOnline.toString(),
+                          ));
+                    });
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
             }));
   }
 }
