ea3dcd4907b3b9db511d143dd1fe737d65dee44c
Luca <auth@luca.cx>
2025-01-12 17:28:42 +0200

    Add headers

--- lib/views/email_input.dart
+++ lib/views/email_input.dart
@@ -1,3 +1,9 @@
+import "dart:convert";
+
+import "package:flutter/material.dart";
+
+import "package:http/http.dart" as http;
+
 class EmailInput extends StatefulWidget {
   const EmailInput({Key? key}) : super(key: key);
 
@@ -8,20 +14,32 @@ class EmailInput extends StatefulWidget {
 class _EmailInputState extends State<EmailInput> {
   final _emailController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  bool _isEmailSent = false;
+  bool _isError = false;
 
   Future<void> _postEmail() async {
     if (_formKey.currentState!.validate()) {
-      final url = Uri.parse('https://your-endpoint.com/email');
-      final response = await http.post(url, body: {'email': _emailController.text});
+      print(_emailController.text);
+
+      final url = Uri.parse("http://192.168.110.37:8000/app/login/otp/send/");
+      final body = {"email": _emailController.text};
+      final response = await http.post(url,
+          headers: {
+            "accept": "*/*",
+            "Content-Type": "application/json",
+          },
+          body: jsonEncode(body));
 
       if (response.statusCode == 200) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Email sent successfully')),
-        );
+        setState(() {
+          _isEmailSent = true;
+          _isError = false;
+        });
       } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Failed to send email')),
-        );
+        setState(() {
+          _isEmailSent = false;
+          _isError = true;
+        });
       }
     }
   }
@@ -47,7 +65,7 @@ class _EmailInputState extends State<EmailInput> {
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Please enter an email';
-                  } else if (!value.contains('@')) {
+                  } else if (!value.contains("@") && !value.contains(".")) {
                     return 'Please enter a valid email';
                   }
                   return null;
@@ -58,6 +76,10 @@ class _EmailInputState extends State<EmailInput> {
                 onPressed: _postEmail,
                 child: const Text('Send Email'),
               ),
+              if (_isEmailSent)
+                const Text("Email sent successfully")
+              else if (_isError)
+                const Text("Failed to send email")
             ],
           ),
         ),
